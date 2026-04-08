acde38abe963f3ddd80b85ffe07b1c58774e5e4d
rickktt <ricagamer125@gmail.com>
2026-04-07 23:17:59 -0600

    Corrección de datos en la conexion a la base de datos

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,13 +1,12 @@
 <?php
-$servidor = "localhost"; //SERVIDOR LOCAL
-$usuario = "root";//USUARIO DEL SERVIDOR DE LA BASE DE DATOS EN PHPMYADMIM
-$contrasena = "root";//CONTRASEÑA POR DEFECTO EN PHPMYADMIN
-$base_datos = "db_estudiantes";//NOMBRE DE LA BASE DE DATOS EN PHPMYADMIN
-$conexion = new mysqli($servidor, $usuario, $contrasena, $base_datos);//INICIALIZAMOS CON MYSQLI LA CONEXION AL SERVIDOR
-//HACEMOS UN MANEJO DE ERRORES A TRAVES DE LA CONDICIONAL
+$servidor = "localhost"; 
+$usuario = "root";
+$contrasena = "";
+$base_datos = "pharmacy_sv";
+$conexion = new mysqli($servidor, $usuario, $contrasena, $base_datos);
+
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-session_start(); //HACEMOS UNA SESION 
+session_start();
 ?>
-
